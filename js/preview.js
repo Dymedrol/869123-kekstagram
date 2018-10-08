4aeac01ac26c87541b75ea7e9db50b4580a3d21d
Dymedrol <Dmitrykob@gmail.com>
2018-10-08 17:59:03 +0700

    Выполнено задание module5-task2

---
+++ js/preview.js
@@ -0,0 +1,52 @@
+'use strict';
+
+(function () {
+
+  window.bigPicture = document.querySelector('.big-picture');
+
+  window.initBigPicture = function () {
+    // Показываем элемент .big-picture
+    window.bigPicture.classList.remove('hidden');
+
+    // Закрываем элемент .big-picture по клику на крестик
+    var bigPictureCancel = document.querySelector('.big-picture__cancel');
+    bigPictureCancel.addEventListener('click', function () {
+      window.bigPicture.classList.add('hidden');
+    });
+    // Заполняем данными
+    window.bigPicture.querySelector('img').src = window.otherUsersPictures[0].url;
+    window.bigPicture.querySelector('.likes-count').textContent = window.otherUsersPictures[0].likes;
+    window.bigPicture.querySelector('.comments-count').textContent = window.otherUsersPictures[0].comments.length;
+    window.bigPicture.querySelector('.social__caption').textContent = window.otherUsersPictures[0].description;
+
+    // генерирует элементы с комментариями
+    var getBigPictureComment = function (comments) {
+
+      var socialComments = [];
+
+      for (var i = 0; i < comments.length; i++) {
+        var socialComment = document.createElement('li');
+        socialComment.classList.add('social__comment');
+        var picture = document.createElement('img');
+        picture.classList.add('social__picture');
+        picture.src = 'img/avatar-' + window.getRandonNumber(1, 6) + '.svg';
+        picture.alt = 'Аватар комментатора фотографии';
+        socialComment.appendChild(picture);
+        var socialText = document.createElement('p');
+        socialText.classList.add('social__text');
+        socialText.textContent = comments[i];
+        socialComment.appendChild(socialText);
+        socialComments.push(socialComment);
+      }
+      return window.getFragment(socialComments);
+    };
+
+    document.querySelector('.social__comments').appendChild(getBigPictureComment(window.otherUsersPictures[0].comments));
+  };
+
+  // убираем блоки счётчика комментариев
+  document.querySelector('.social__comment-count').classList.add('visually-hidden');
+  // убираем блоки загрузки новых комментариев
+  document.querySelector('.comments-loader').classList.add('visually-hidden');
+
+})();
